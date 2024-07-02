6700c0f63a5fccd25e656a56494ca7f7b0444aaa
michele.puricelli <michele.puricelli@bcode.cloud>
2024-07-02 23:08:13 +0200

    fix: minor fixes

--- src/components/CityInput.tsx
+++ src/components/CityInput.tsx
@@ -1,42 +1,51 @@
 import React from "react";
-import { IconSearch } from "../icons";
+import { IconSearch, Loader } from "../icons";
 import { useCityWeather } from "../contexts/CityWeatherContext";
 
 const CityInput = () => {
   const { city, setCity, isLoading, refetchCity, error } = useCityWeather();
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCity(e.target.value);
+  };
+
+  const renderInput = () => (
+    <input
+      value={city}
+      onChange={handleChange}
+      className="w-full border-black border-2 p-2.5 focus:outline-none focus:shadow-[2px_2px_0px_rgba(0,0,0,1)] focus:bg-[#FFA6F6] active:shadow-[2px_2px_0px_rgba(0,0,0,1)]"
+      placeholder="Enter city name or address..."
+    />
+  );
+
+  const renderButton = () => (
+    <button
+      onClick={refetchCity}
+      disabled={!city || isLoading}
+      className="border-black border-2 disabled:bg-[#FFA6D6] bg-[#FFA6F6] hover:bg-[#fa8cef] active:bg-[#f774ea] w-12 h-12 flex items-center justify-center"
+    >
+      {isLoading ? (
+        <Loader className="w-6 h-6 animate-spin" />
+      ) : (
+        <IconSearch className="w-6 h-6" />
+      )}
+    </button>
+  );
+
+  const renderError = () =>
+    error && (
+      <p>Error: {error.message ? error.message : "Something wrong happened"}</p>
+    );
 
   return (
     <span className="flex flex-row gap-2">
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
+      {
         <>
-          <input
-            value={city}
-            onChange={handleChange}
-            className="w-full border-black border-2 p-2.5 focus:outline-none focus:shadow-[2px_2px_0px_rgba(0,0,0,1)] focus:bg-[#FFA6F6] active:shadow-[2px_2px_0px_rgba(0,0,0,1)]"
-            placeholder="Enter city name"
-          />
-          <button
-            onClick={() => refetchCity()}
-            disabled={city === ""}
-            className="border-black border-2 disabled:bg-[#FFA6D6] bg-[#FFA6F6] hover:bg-[#fa8cef] active:bg-[#f774ea] w-12 h-12 flex items-center justify-center"
-          >
-            <IconSearch className="w-6 h-6" />
-          </button>
-
-          {error && (
-            <p>
-              Error:{" "}
-              {error && error.message
-                ? error.message
-                : "Something wrong happened"}
-            </p>
-          )}
+          {renderInput()}
+          {renderButton()}
+          {renderError()}
         </>
-      )}
+      }
     </span>
   );
 };
--- src/icons.ts
+++ src/icons.ts
@@ -1,5 +1,7 @@
 import { IoSearch as IconSearch } from "react-icons/io5";
+import { BiLoaderCircle as Loader } from "react-icons/bi";
 
 export {
-    IconSearch
+    IconSearch,
+    Loader
 }
